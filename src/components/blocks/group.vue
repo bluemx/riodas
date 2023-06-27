5261df2d83ff5df720d3cde577753f15f63a06c8
Albin Rodriguez <ealbinu@gmail.com>
2023-06-27 12:11:57 -0600

    update

--- src/components/blocks/group.vue
+++ src/components/blocks/group.vue
@@ -18,30 +18,32 @@ watch(()=>props, ()=>{
 const cssbackgroundFN = () => {
     const bg = props?.data?.background
     let stylerule = ''
-    if(bg.includes('http')){
-        //External?
-        var image = new Image();
-        image.crossOrigin = "anonymous";
-        image.onload = function() {
-            var canvas = document.createElement("canvas");
-            canvas.width = image.width;
-            canvas.height = image.height;
-            var context = canvas.getContext("2d");
-            context.drawImage(image, 0, 0);
-            var base64 = canvas.toDataURL("image/webp");
-            block.value.style.backgroundImage = "url(" + base64 + ")";
+    if(bg){
+        if(bg.includes('http')){
+            //External?
+            var image = new Image();
+            image.crossOrigin = "anonymous";
+            image.onload = function() {
+                var canvas = document.createElement("canvas");
+                canvas.width = image.width;
+                canvas.height = image.height;
+                var context = canvas.getContext("2d");
+                context.drawImage(image, 0, 0);
+                var base64 = canvas.toDataURL("image/webp");
+                block.value.style.backgroundImage = "url(" + base64 + ")";
 
-            //cssbackground.value = "background-image:url(" + image.src + ")";
-            //console.log(block.value, 'external', image)
-            //block.value.append(image)
-            setTimeout(()=>{
-                //image.style.backgroundImage = "url(" + image.src + ")";
-            }, 100)
-        };
-        image.src = bg;
-    } else {
-        //Local
-        stylerule = 'background-image:url(/ODAS/'+oda.odaID+'/'+bg+');'
+                //cssbackground.value = "background-image:url(" + image.src + ")";
+                //console.log(block.value, 'external', image)
+                //block.value.append(image)
+                setTimeout(()=>{
+                    //image.style.backgroundImage = "url(" + image.src + ")";
+                }, 100)
+            };
+            image.src = bg;
+        } else {
+            //Local
+            stylerule = 'background-image:url(/ODAS/'+oda.odaID+'/'+bg+');'
+        }
     }
 }
 onMounted(() => {
