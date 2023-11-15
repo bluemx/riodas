771c86ab979c32ba273f33030acb3c91575a5d42
Albin Rodriguez <ealbinu@gmail.com>
2023-11-15 09:53:09 -0600

    update audio listener

--- src/components/blocks/elashq.vue
+++ src/components/blocks/elashq.vue
@@ -1,7 +1,9 @@
 <template>
+<div>
     <div v-for="(item, index) in items" :key="index">
-        <ElashqItem :information="item.information" :question="item.question" :options="item.options" :answer="item.answer" :counter="index+1" />
+        <ElashqItem :blockindex="blockindex+'-'+'elashqitem'" :audiofile="audiofileclean" :information="item.information" :question="item.question" :options="item.options" :answer="item.answer" :counter="index+data.counter" />
     </div>
+</div>
 </template>
 <script setup>
 import { useOda } from '../../store/oda';
@@ -12,6 +14,7 @@ deepdash(_)
 
 
 const props = defineProps({
+    blockindex: String,
     data: Object,
     blockindex: String,
     name: String
@@ -19,157 +22,16 @@ const props = defineProps({
 
 const oda = useOda()
 
-const fullobject = ref({})
-const ready = ref(false)
-
-const dropcounter = ref(0)
 const items = ref([])
-const texts = ref([])
-const itemsobj = ref([])
-const textsobj = ref()
+const audiofileclean = ref(null)
+
 
 watch(()=>props.data, ()=>{ init() }, {deep:true})
 
 
 
-const getRandomCharacters=_=>"xxxx".replace(/x/g,_=>"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[Math.random()*62|0]);
-
-const MGK = "-"+getRandomCharacters()+"-"
 
 
-const genItems = () => {
-    items.value.forEach((item)=>{
-        const itemarr = item.split(',')
-        const arraobjs = itemarr.map((i)=>{
-            return {
-                text: i,
-                block: "text",
-                class: ""+ (props.data.classitem || '')
-            }
-        })
-        itemsobj.value.push(arraobjs)
-    })
-}
-
-
-const groupCounter = {
-    "block": "text", "text": "1.", "class": "mr-3 text-xl bg-slate-400/10 px-2 rounded text-slate-400 " + (props.data.classcounter || '')
-}
-const groupText = {
-    "block": "text", "class": "", "text": "texto"
-}
-const groupOptions = {
-    "block": "group", "class": "flex gap-3 my-4 [&>*:nth-child(odd)]:bg-white [&>*:nth-child(even)]:bg-slate-200 [&>*]:p-2 [&>*]:rounded",
-    "content": [],
-}
-
-const groupJson = { "marker": "▶️", "class": "my-5 "+ (props.data.classtexts || ''), "content": [], "block": "group" };
-
-
-
-
-
-
-
-
-
-const genTexts = () => {
-    let groups = []      
-    texts.value.forEach((item, index)=>{
-        let content = JSON.parse(JSON.stringify(groupJson))
-        content.name = content.id = "groupJson-"+index
-
-        if(props.data.counter){
-            content.content.push({
-                ...groupCounter,
-                text: (index+1)+'.',
-                name: 'enum'+index,
-                id: 'enum'+index,
-            })
-        }
-        //TEXT
-        item.forEach((element, eIndex) => {
-            if(typeof element === 'string'){
-                content.content.push({
-                    ...groupText,
-                    text: element,
-                    name: 'groupText'+index+eIndex,
-                    id: 'groupText'+index+eIndex,
-                })
-
-            } else if(typeof element === 'boolean'){
-                
-                let thecontent = JSON.parse(JSON.stringify(groupOptions.content[0]))
-                //thecontent.positive = 'worddrop'+dropcounter.value
-                thecontent.name = thecontent.id = 'groupOptionsContent'+index+eIndex
-                thecontent.options = itemsobj.value[dropcounter.value]
-                let groupOptionsObj = JSON.parse(JSON.stringify(groupOptions))
-                groupOptionsObj.name = groupOptionsObj.id = 'groupOptions'+index+eIndex
-                groupOptionsObj.content = [thecontent]
-
-                content.content.push(groupOptionsObj)
-
-                dropcounter.value++
-            }
-        });
-
-
-        groups.push(content)
-    })
-   
-
-  fullobject.value.content.push(...groups)
-
-
-}
-
-
-
-
-
-
-
-
-const splitStringWithUnderscore = (str) => {
-  const parts = str.split('_');
-  const result = [];
-  for (let i = 0; i < parts.length; i++) {
-    result.push(parts[i]);
-    if (i < parts.length - 1) {result.push(true);}
-  }
-  return result;
-}
-
-
-
-
-const replaceobjectinoda = () => {
-    _.eachDeep(oda.oda.activity, (valueD, keyD, parentD, ctxD) => {
-        if(keyD=='name' && valueD==props.name){
-            _.set(oda.oda.activity, ctxD._item.parent.path, fullobject.value);
-        }
-    })
-
-}
-
-/*
-function textToJSON(text) {
-  // Split the input into groups
-  const groups = text.trim().split(/\n\s*\n/);
-  const items = [];
-
-  // Process each group
-  groups.forEach(group => {
-    const lines = group.split('\n');
-    const item = {};
-    item.question = lines[0];
-    item.options = lines.slice(1, -1); // get all lines except first and last
-    item.answer = lines[lines.length - 1]; // get the last line
-    items.push(item);
-  });
-  return items;
-}
-*/
 function textToJSON(text) {
   // Split the input into groups
   const groups = text.trim().split(/\n\s*\n/);
@@ -200,109 +62,21 @@ function textToJSON(text) {
 
 
 
-const indexToABC = (index) =>{
-    const abc = 
-        index==0?'A':'' +
-        index==1?'B':'' +
-        index==2?'C':'' +
-        index==3?'D':'' +
-        index==4?'E':'' +
-        index==5?'F':'' 
-    return abc
-}
-
-const createJSONS = () => {
-    fullobject.value.content = []
-    
-    items.value.forEach((item,index)=>{
-        let mainGroup = JSON.parse(JSON.stringify(groupJson))
-        mainGroup.name = mainGroup.id = "mainGroup"+MGK+index
-        mainGroup.class = 'p-2 rounded shadow-xl my-2 border-2 border-white/50'
-        
-        // #### introGroup
-        let introGroup = JSON.parse(JSON.stringify(groupJson))
-        introGroup.name = introGroup.id = "mainIntroGroup"+MGK+index
-        
-        // INFORMATION
-        if(item.information){
-            var iteminfo = item.information
-            iteminfo = iteminfo.replace(/\\n/g, '<br>');
-            console.log(iteminfo)
-            introGroup.content.push({
-                ...groupText,
-                text: iteminfo,
-                name: 'int'+MGK+index,
-                id: 'int'+MGK+index,
-                class: "block whitespace-pre-wrap rounded bg-sky-100 w-full p-2",
-            })
-        }
-
-        // Add intro to mainGroup
-        mainGroup.content.push(introGroup)
-        
-
-        // #### contentGroup
-        let contentGroup = JSON.parse(JSON.stringify(groupJson))
-        contentGroup.name = contentGroup.id = "mainContentGroup"+MGK+index
-        
-        
-
-        //COUNTER
-        if(props.data.counter>0){
-            contentGroup.content.push({
-                ...groupCounter,
-                text: (index+props.data.counter)+'.',
-                name: 'counter'+MGK+index,
-                id: 'counter'+MGK+index,
-            })
-        }
-        //QUESTION
-        contentGroup.content.push({
-            ...groupText,
-            text: item.question,
-            name: 'q'+MGK+index,
-            id: 'q'+MGK+index,
-            class: "text-xl"
-        })
-        //OPTIONS
-        contentGroup.content.push({
-            ...groupOptions,
-            content: item.options.map((op,index)=>{
-                return {
-                    ...groupText,
-                    text: "<b>"+indexToABC(index)+")</b> "+ op,
-                    name: 'oi'+MGK+index,
-                    id: 'oi'+MGK+index,
-                }
-            }),
-            name: 'ops'+MGK+index,
-            id: 'ops'+MGK+index,
-        })
-
-        // Add content to mainGroup
-        mainGroup.content.push(contentGroup)
-
-        //Add to fullobject
-        fullobject.value.content.push(mainGroup)
-    })
-
-    //replaceobjectinoda()
-}
-
 
 const init = () => {
-    ready.value = false
-    fullobject.value = { block: "group", content:[], class: props.data.class || '' }
-    itemsobj.value = []
-    dropcounter.value = 0
-
-    // ITEMS
-
     items.value = textToJSON(props.data.textual)
+    if(props.data.audiofiles) {
+        audiofileclean.value =  removeFileNameFromURL(props.data.audiofiles)
+        console.log('clean:::', audiofileclean.value)
+    }
+}
 
-    createJSONS()
 
-    
+function removeFileNameFromURL(urlString) {
+  const parts = urlString.split('/');
+  const filename = parts.pop();
+  const urlWithoutFilename = parts.join('/');
+  return urlWithoutFilename.endsWith('/') ? urlWithoutFilename : urlWithoutFilename + '/';
 }
 
 
--- src/components/blocks/elashqItem.vue
+++ src/components/blocks/elashqItem.vue
@@ -1,68 +1,88 @@
 <template>
 <div>
-<div data="elashqitem" class="p-5 my-10 mx-4 rounded-lg shadow-xl border-4   relative transition ease-out duration-200" :class="input!==null?'border-secondary bg-secondary text-white':'border-warning/20 bg-slate-50'">
-    <div data="eqi-counter" class="rounded-full shadow-lg flex justify-center items-center aspect-square w-10 absolute -left-5 -top-5  transition-all" :class="input!==null?'bg-neutral':'bg-warning'">
-        <div class="font-bold text-white">{{ counter }}</div>
-    </div>
-    <!--information-->
-    <div v-if="information" class="w-5/6 mx-auto">
-        <div data="eqi-information" class="block whitespace-pre-wrap rounded-xl bg-info p-4 text-neutral " v-html="information"></div>
-        <div class="w-6 ml-10 -mt-0.5">
-            <svg viewBox="0 0 156 157" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M155.999 156.961C135.513 156.961 115.227 152.926 96.3004 145.086C77.3735 137.246 60.1762 125.755 45.6903 111.269C31.2043 96.7835 19.7134 79.5862 11.8736 60.6594C4.03387 41.7326 -0.00119084 21.447 -0.00119019 0.960722C-0.00119019 0.960722 115.999 0.960657 155.999 0.960726C118 34 118.662 104.252 155.999 156.961Z" fill="#A6EDE2"/>
-                </svg>
-
+    <div v-if="!loading" data="elashqitem" class="p-5 my-10 mx-4 rounded-lg shadow-xl border-4   relative transition ease-out duration-200" :class="input!==null?'border-secondary bg-secondary text-white':'border-warning/20 bg-slate-50'">
+        <div data="eqi-counter" class="rounded-full shadow-lg flex justify-center items-center aspect-square w-10 absolute -left-5 -top-5  transition-all" :class="input!==null?'bg-neutral':'bg-warning'">
+            <div class="font-bold text-white">{{ counter }}</div>
         </div>
+        <!-- 🔕🔕🔕🔕 NO AUDIO -->
+        <template v-if="audiofile===null">
+            <!--information-->
+            <div v-if="information" class="w-5/6 mx-auto">
+                <div data="eqi-information" class="block whitespace-pre-wrap rounded-xl bg-info p-4 text-neutral " v-html="information"></div>
+                <div class="w-6 ml-10 -mt-0.5">
+                    <svg viewBox="0 0 156 157" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M155.999 156.961C135.513 156.961 115.227 152.926 96.3004 145.086C77.3735 137.246 60.1762 125.755 45.6903 111.269C31.2043 96.7835 19.7134 79.5862 11.8736 60.6594C4.03387 41.7326 -0.00119084 21.447 -0.00119019 0.960722C-0.00119019 0.960722 115.999 0.960657 155.999 0.960726C118 34 118.662 104.252 155.999 156.961Z" fill="#A6EDE2"/>
+                        </svg>
+                </div>
+            </div>
+            
+            <!-- question -->
+            <div data="eqi-question" class="text-2xl py-10 text-center" v-if="question!=='-'">{{ question }}</div>
 
-    </div>
-    
-    <!-- question -->
-    <div data="eqi-question" class="text-2xl py-10 text-center" v-if="question!=='-'">{{ question }}</div>
-
-    <!-- options -->
-    <div data="eqi-options" class="border-4 bg-slate-300/40 border-slate-300 p-2 rounded-lg flex flex-wrap flex-col gap-5  justify-center  [&>*]:p-2 [&>*]:rounded">
-        <template v-for="(item, index) in options" :key="index">
-            <div class="flex gap-1 items-center bg-white text-base-100">
-                <div class="font-bold opacity-75 text-sm w-6 aspect-square bg-slate-100 flex justify-center items-center rounded">{{indexToABC(index)}})</div>
-                <div>{{ item }}</div>
+            <!-- options -->
+            <div data="eqi-options" class="border-4 bg-slate-300/40 border-slate-300 p-2 rounded-lg flex flex-wrap flex-col gap-5  justify-center  [&>*]:p-2 [&>*]:rounded">
+                <template v-for="(item, index) in options" :key="index">
+                    <div class="flex gap-1 items-center bg-white text-base-100">
+                        <div class="font-bold opacity-75 text-sm w-6 aspect-square bg-slate-100 flex justify-center items-center rounded">{{indexToABC(index)}})</div>
+                        <div>{{ item }}</div>
+                    </div>
+                </template>
+            </div>
+            <div>
+                <div class="w-6 mx-auto">
+                    <svg viewBox="0 0 88 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88 0L44 44L0 0H88Z" fill="#CBD5E1"/></svg>
+                </div>
             </div>
         </template>
-    </div>
-    <div>
-        <div class="w-6 mx-auto">
-            <svg viewBox="0 0 88 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88 0L44 44L0 0H88Z" fill="#CBD5E1"/></svg>
-        </div>
-    </div>
-    <!-- answers-->
-    <div data="eqi-answers"  class="flex flex-wrap justify-center items-center gap-5 mt-5">
-        <template v-for="(item, index) in options" :key="index">
-            <div :class="['w-16 rounded-full aspect-square flex justify-center items-center cursor-pointer  hover:shadow-sm hover:translate-y-0 transition-all border-2 box-content', input===index?'bg-neutral scale-125 ease-out':' border-white -translate-y-0.5 shadow-xl']" @click="onChange($event,index)">
-                <div class="font-bold text-2xl">{{ indexToABC(index) }}</div>
+        <!-- 🔔🔔🔔🔔 WITH AUDIO -->
+        <template v-else>
+            <div v-if="audioblock">
+                <div class=" border-4 bg-slate-100/80 border-slate-300 p-2 rounded-lg">
+                    <Content :data="audioblock" :blockindex="blockindex+'-'+counter+'-audio'"></Content>
+                </div>
+                <div>
+                <div class="w-6 mx-auto">
+                    <svg viewBox="0 0 88 44" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88 0L44 44L0 0H88Z" fill="#CBD5E1"/></svg>
+                </div>
+            </div>
+
             </div>
         </template>
+        <!-- answers-->
+        <div data="eqi-answers"  class="flex flex-wrap justify-center items-center gap-5 mt-5">
+            <template v-for="(item, index) in options" :key="index">
+                <div :class="['w-16 rounded-full aspect-square flex justify-center items-center cursor-pointer  hover:shadow-sm hover:translate-y-0 transition-all border-2 box-content', input===index?'bg-neutral scale-125 ease-out':' border-white -translate-y-0.5 shadow-xl']" @click="onChange($event,index)">
+                    <div class="font-bold text-2xl">{{ indexToABC(index) }}</div>
+                </div>
+            </template>
+        </div>
+
+
     </div>
-
-
-</div>
-<template v-if="counter%10===0">
-    <Divider :class="['max-w-md mx-auto !my-10']"></Divider>
-</template>
+    <template v-if="counter%10===0">
+        <Divider :class="['max-w-md mx-auto !my-10']"></Divider>
+    </template>
 </div>
 </template>
 
 <script setup>
 import ShapesAnimation from '../all/ShapesAnimation';
 import Divider from '../utilities/divider.vue';
+import Content from './content.vue';
 
 const props = defineProps({
+    blockindex: String,
     information: String,
     counter: Number,
     question: String,
     options: Array,
     answer: String,
+    audiofile: String
 })
-
+const loading = ref(false)
 const input  = ref(null)
+
+const audioblock = ref(null)
 
 const indexToABC = (index) =>{
     const abc = 
@@ -76,8 +96,33 @@ const indexToABC = (index) =>{
 }
 
 const onChange = ($event, index) => {
-    input.value = index
-    ShapesAnimation.playcircle($event.target)
+    if(input.value!==null){
+        input.value = null
+        setTimeout(()=>{
+            input.value = index
+            ShapesAnimation.playcircle($event.target)
+        }, 200)
+    } else {
+        input.value = index
+        ShapesAnimation.playcircle($event.target)
+    }
 }
+
+
+const init = () => {
+    audioblock.value = 
+    {
+        "block": "audio",
+        "class": "",
+        "file": props.audiofile+"a"+props.counter+".mp3",
+        "autoplay": false
+    }
+}
+
+onMounted(() => {
+    init()
+})
+watch(()=>props, ()=>{ init() }, {deep:true})
+
 
 </script>
